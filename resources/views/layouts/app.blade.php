b1208f959d70d8490f9bfdab2da1c590c2172a18
Muadzizizulala02 <muadzkhalid6@gmail.com>
2025-12-30 12:16:48 +0800

    Setup Railway

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    {{-- FAVICON LINK --}}
+    <link rel="icon" href="{{ asset('blogging.png') }}">
     
     {{-- Page title - defaults to "Blog Platform" --}}
     <title>{{ $title ?? 'Blog Platform' }}</title>
